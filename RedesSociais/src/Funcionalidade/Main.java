9ad8153d8de2dc21cf07e4db96f368fa3e6ec8fd
Antonio <antoninho.ab.silva@gmail.com>
2024-05-27 12:27:23 -0300

    Modifiquei o arquivo com comentários

--- RedesSociais/src/Funcionalidade/Main.java
+++ RedesSociais/src/Funcionalidade/Main.java
@@ -3,11 +3,14 @@ import Exceptions.Unchecked;
 
 public class Main {
     public static void main(String[] args) {
+
+        //Instanciando as redes sociais
         Facebook facebook = new Facebook();
         Twitter twitter = new Twitter();
         Instagram instagram = new Instagram();
         GooglePlus googlePlus = new GooglePlus();
 
+        //Alterando os atributos
         facebook.senha = "f12345";
         facebook.numAmigos = 150;
 
@@ -20,15 +23,19 @@ public class Main {
         googlePlus.senha = "g12345";
         googlePlus.numAmigos = 70;
 
+        //Instanciando o array de redes sociais
         RedeSocial[] redes = new RedeSocial[2];
         redes[0] = googlePlus;
         redes[1] = twitter;
 
+        //Instanciando o usuário
         Usuario usuario = new Usuario(redes);
 
+        //Definindo os atributos
         usuario.setNome("Antonio");
         usuario.setEmail("antonio.silva@ges.inatel.br");
 
+        //Try-catch para realizar a verificação de exceção, e imprimindo os métodos das redes sociais do usuário
         try{
             ((GooglePlus) usuario.getRedes()[0]).fazStreaming();
             usuario.getRedes()[0].postarVideo();
